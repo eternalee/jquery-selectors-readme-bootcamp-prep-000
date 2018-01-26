0cf8f61b69df038525f7f3352eb7784c32ecd642
Eterna <one.eternity@gmail.com>
2018-01-26 17:47:24 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
-// declare your functions here...
+function paragraphSelector() {
+  $('p')
+}
+
+function lastImageSelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('div .pics')
+}
+
+function firstListItem() {
+  $('ul li #pic-list:first-child')
+}
+
